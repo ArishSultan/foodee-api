0283914e132592b02b10e15486c003a6677cb510
Yasir-Ameen <amaanrajput940@rubikkube.com>
2019-03-08 17:17:41 +0500

    category

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -12,7 +12,7 @@ class ProfileController extends Controller
     public function index() {
     }
 
-    public function show(User $user, Request $request) {
+    public function show(User $user) {
 //        $user = $request->user();
         return $user;
     }
