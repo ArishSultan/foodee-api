d07eecc8d6ff75bbd8a261eab402746787b9081e
Yasir-Ameen <amaanrajput940@rubikkube.com>
2019-03-18 15:47:21 +0500

    comment

--- app/Like.php
+++ app/Like.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Like extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'post_id',
+        'user_id',
+    ];
 }
