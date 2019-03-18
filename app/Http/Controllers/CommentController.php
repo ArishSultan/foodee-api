ba0524e850e0ef1a9575543d584bf93a05acbab7
Yasir-Ameen <amaanrajput940@rubikkube.com>
2019-03-18 13:24:47 +0500

    comment

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -10,7 +10,7 @@ use App\User;
 class CommentController extends Controller
 {
     public function index(Request $request) {
-        $comments = Comment::orderBy('created_at', 'asc')->where('post_id', $request->post_id)->with('user')->paginate(6);
+        $comments = Comment::orderBy('created_at', 'asc')->where('post_id', $request->post_id)->paginate(6)->with('user');
         return $comments;
     }
     public function show($id) {
