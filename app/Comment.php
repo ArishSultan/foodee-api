28d065cc49cc9c828bda2005f8dd24b9245df49d
Yasir-Ameen <amaanrajput940@rubikkube.com>
2019-03-18 13:11:14 +0500

    comment

--- app/Comment.php
+++ app/Comment.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'post_id',
+        'user_id',
+        'content'
+    ];
 }
