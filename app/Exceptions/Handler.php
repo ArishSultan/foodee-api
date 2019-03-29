a544140b20ec35e72d50712bafeacc0b352134bc
Yasir-Ameen <amaanrajput940@rubikkube.com>
2019-03-29 18:01:22 +0500

    env

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -56,7 +56,7 @@ class Handler extends ExceptionHandler
                 "status" => false,
                 "statusCode" => 422,
                 "message" =>  "The given data was invalid.",
-                "result" => $e->errors()
+                "errors" => $e->errors()
             ], 422);
         }else{
             parent::convertValidationExceptionToResponse($e, $request);
