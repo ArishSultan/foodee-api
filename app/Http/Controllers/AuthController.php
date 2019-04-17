876ebe6814db60793126d77fd351c1c451316704
Yasir-Ameen <amaanrajput940@rubikkube.com>
2019-04-17 16:54:15 +0500

    changes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -130,7 +130,7 @@ class AuthController extends Controller
         $user->lng = $request->lng;
         if($user->save()){
             $users = DB::select(DB::raw("SELECT
-  id, (
+  id, username, email, phone, lat, lng (
     3959 * acos (
       cos ( radians($request->lat) )
       * cos( radians( lat ) )
