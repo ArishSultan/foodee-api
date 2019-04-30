973e09c3c8329bcd8047d3e781858ec3d8a64dfe
Yasir-Ameen <amaanrajput940@rubikkube.com>
2019-04-30 19:10:14 +0500

    check has food or not

--- app/Http/Controllers/FoodCategoryController.php
+++ app/Http/Controllers/FoodCategoryController.php
@@ -12,7 +12,7 @@ class FoodCategoryController extends Controller
     public function index(Request $request) {
 //        return FoodCategory::orderByRaw("RAND()")->get();
         $user = $request->user();
-        return $user->profile->foods()->orderBy('created_at')->get();
+        return $user->profile->foods()->orderBy('created_at', 'desc')->get();
     }
 
     public function show(FoodCategory $food) {
