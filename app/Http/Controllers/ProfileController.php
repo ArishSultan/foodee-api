1f61b0f798b1fa6cb7f857d9c664ba8d872dc59f
Yasir-Ameen <amaanrajput940@rubikkube.com>
2019-04-30 20:54:33 +0500

    check has food or not

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Carbon\Carbon;
 use App\User;
+use Illuminate\Support\Facades\DB;
 
 class ProfileController extends Controller
 {
