eec9b6f25f84f3631eed715a5adfeff4a3d8d5be
Yasir-Ameen <amaanrajput940@rubikkube.com>
2019-05-01 17:52:39 +0500

    filter

--- app/Http/Controllers/FilterController.php
+++ app/Http/Controllers/FilterController.php
@@ -10,6 +10,8 @@ use App\User;
 class FilterController extends Controller
 {
     public function index(Request $request) {
-        return $request->all();
+        $food = $request->query('food');
+        $contribution = $request->query('contribution');
+        return $food." - ".$contribution;
     }
 }
