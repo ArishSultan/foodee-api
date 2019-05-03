4be202a9676cf4188e0f1cbc576ef93877d21368
Yasir-Ameen <amaanrajput940@rubikkube.com>
2019-05-03 16:38:45 +0500

    confirmation:

--- resources/views/emails/user/confirmation.blade.php
+++ resources/views/emails/user/confirmation.blade.php
@@ -5,7 +5,15 @@
         Please click this button to confirm your Foodee account:
     </p>
 
-    <center><a href="" class="btn btn-lg btn-primary">Confirm your account</a> </center>
+    <center><a href="http://34.220.151.44/account/confirm/{{$data['email']}}/{{$data['uid']}}" class="btn btn-lg btn-primary" style="background: #56b656;
+
+padding: 15px;
+
+font-size: 21px;
+
+color: #fff;
+
+border-radius: 4px;">Confirm your account</a> </center>
 
     <p><a href="http://34.220.151.44/account/confirm/{{$data['email']}}/{{$data['uid']}}">http://34.220.151.44/account/confirm/{{$data['email']}}/{{$data['uid']}}</a> </p>
 
