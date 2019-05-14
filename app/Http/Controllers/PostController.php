c4bd8c1f26e0912acfab93834ac5f909090bfd95
Yasir-Ameen <amaanrajput940@rubikkube.com>
2019-05-14 17:19:01 +0500

    files upload in news feed

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -23,7 +23,7 @@ class PostController extends Controller
         if($request->hasFile('photos')){
             $photos = UploadServiceProvider::multiUploads($request, 'post');
             $request['photos'] = $photos;
-            return $photos;
+            return $request->all();
         }
 
         $post = NewsFeed::create($request->all());
