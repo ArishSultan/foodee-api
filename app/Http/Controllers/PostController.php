34d205d3d8596c9202c0a470347160743f7f99e7
Yasir-Ameen <amaanrajput940@rubikkube.com>
2019-05-14 17:29:45 +0500

    files upload in news feed

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -17,16 +17,17 @@ class PostController extends Controller
         return $post;
     }
     public function store(Request $request) {
+        global  $photos_string;
         $request['user_id'] = $request->user()->id;
 //        $photos = ['http://www.erro-shop.de/WebRoot/Store7/Shops/61191284/4B6C/0809/A63D/3582/ECF1/C0A8/28BC/0D81/72370_thunfisch_salat_ohne_teller_ml.jpg', 'https://ae01.alicdn.com/kf/HTB1ico.a9YTBKNjSZKbq6xJ8pXaC/LeadingStar-6pcs-Pack-Cute-Imitation-Food-Erasers-Hamburger-Eraser-Office-Study-Correction-Supplies-for-Students-OL.jpg_640x640.jpg'];
 //        $request['photos'] = implode(",", $photos);
         if($request->hasFile('photos')){
             $photos = UploadServiceProvider::multiUploads($request, 'post');
-            $request['photos'] = implode(",", $photos);
+            $photos_string = implode(",", $photos);
 //            return dd(implode(",", $photos));
         }
 
-        $post = NewsFeed::create($request->all());
+        $post = NewsFeed::create(["user_id"=>$request->user()->id, "content"=>$request['content'], "photos"=>$photos_string]);
         return response()->json($post, 201);
     }
     public function update(NewsFeed $post, Request $request) {
