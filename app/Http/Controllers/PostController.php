bfcc6418fc292aca8a22c919f46f79c8d9d70310
Yasir-Ameen <amaanrajput940@rubikkube.com>
2019-05-17 12:28:16 +0500

    user obj

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,7 @@ class PostController extends Controller
 {
     public function index() {
 
-        return NewsFeed::withCount('comments')->with(['user'=>function($q) { $q->select('username'); }], 'comments')->orderBy('created_at', 'desc')->paginate(6);
+        return NewsFeed::withCount('comments')->with('user.profile', 'comments')->orderBy('created_at', 'desc')->paginate(6);
 
     }
     public function show(NewsFeed $post) {
