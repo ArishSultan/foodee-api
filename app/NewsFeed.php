34df78b28e54e05a0e7932a62add87c361897fcf
Yasir-Ameen <amaanrajput940@rubikkube.com>
2019-05-17 12:18:39 +0500

    user obj

--- app/NewsFeed.php
+++ app/NewsFeed.php
@@ -23,7 +23,7 @@ class NewsFeed extends Model
 
     public function user()
     {
-        return $this->belongsTo('App\User', 'user_id')->select('username');
+        return $this->belongsTo('App\User', 'user_id')->select('username')->first();
     }
 
     public function likes()
