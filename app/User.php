bb89e8762ec06acc334949ddef65f5fe14702b74
Yasir-Ameen <amaanrajput940@rubikkube.com>
2019-07-04 16:20:45 +0500

    notification center

--- app/User.php
+++ app/User.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Facades\Auth;
 use Laravel\Passport\HasApiTokens;
 class User extends Authenticatable
 {
@@ -21,9 +22,30 @@ class User extends Authenticatable
         'password',
     ];
 
+    protected $appends = ['notifications'];
+
+    public function getNotificationsAttribute($value) {
+
+        if (Auth::user()) {   // Check is user logged in
+            // do stuff
+            $notifications = Auth::user()->notifications;
+            if ($notifications){
+                return $notifications;
+            } else {
+                return [];
+            }
+        }
+
+    }
+
     public function profile()
     {
         return $this->hasOne(Profile::class, 'user_id');
+    }
+
+    public function notifications()
+    {
+        return $this->hasMany(Notification::class, 'user_id');
     }
 
     public function likes()
