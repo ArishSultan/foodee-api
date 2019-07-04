d849d748a599a8bed4b2a215a2c4092d79653364
Yasir-Ameen <amaanrajput940@rubikkube.com>
2019-07-04 16:44:48 +0500

    notification center

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -1,17 +1,20 @@
 <?php
 namespace App\Http\Controllers;
+use App\Helpers\CustomBroadcaster;
+use App\Like;
 use App\NewsFeed;
-use App\Profile;
+use App\Notification;
 use App\Providers\UploadServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Carbon\Carbon;
 use App\User;
-use Illuminate\Support\Facades\DB;
 
 class NotificationController extends Controller
 {
     public function index(Request $request) {
         $user = $request->user();
+        return $user->notifications;
     }
+
 }
