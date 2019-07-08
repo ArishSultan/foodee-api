34338dbe5809b4fec58df5ab3e8b5821f8935277
Yasir-Ameen <amaanrajput940@rubikkube.com>
2019-07-08 17:27:06 +0500

    food check

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -163,7 +163,10 @@ class AuthController extends Controller
     {
 //        $user = $request->user();
         $user = User::where('id', $id)->first();
-        $user['profile'] = $user->profile->foods;
+        if(isset($user->profile->foods)){
+            $user['profile'] = $user->profile->foods;
+        }
+
         return response()->json($user);
     }
     
