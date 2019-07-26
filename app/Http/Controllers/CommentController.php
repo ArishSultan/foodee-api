fe60c2f632576156291c3cf3940673fe57c9736d
Yasir-Ameen <amaanrajput940@rubikkube.com>
2019-07-26 12:45:41 +0500

    comment socket

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -34,7 +34,7 @@ class CommentController extends Controller
             $notification->post_id = $post->id;
             $notification->author_id = $post->user->id;
             $notification->user_id = $request->user()->id;
-//        $notification->message = $request->user()->username. " commented on your post";
+//          $notification->message = $request->user()->username. " commented on your post";
             $notification->message = " commented on your post";
             $notification->type = 2;
             $notification->save();
