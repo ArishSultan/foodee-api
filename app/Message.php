ad17e511dbeba554dbc49d00edcb27afe1bcb2f7
Yasir-Ameen <amaanrajput940@rubikkube.com>
2019-08-08 15:44:25 +0500

    unread

--- app/Message.php
+++ app/Message.php
@@ -12,6 +12,21 @@ class Message extends Model
         "to_id"
     ];
 
+    protected $appends = ['new_msg_count'];
+
+
+    public function getNewMsgCountAttribute($value) {
+//        $temp = $value.split(",");
+            // do stuff
+            $unreadCount = MessageRecipient::where('message_id', $this->id)->where('is_read', 0)->count();
+            if (count($unreadCount) > 0){
+                return $unreadCount;
+            } else {
+                return 0;
+            }
+
+    }
+
     /*
      * Each chat has many messages
      */
