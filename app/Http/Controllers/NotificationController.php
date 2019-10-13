24b191f50da85d8a88c97f3337555fe7f119b117
Yasir-Ameen <amaanrajput940@rubikkube.com>
2019-10-13 16:15:52 +0500

    chat deletd

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -39,8 +39,11 @@ class NotificationController extends Controller
     {
 
         $user = $request->user();
-        $delete = Notification::where('author_id', $user->id)->delete();
+        // $delete = Notification::where('author_id', $user->id)->delete();
+        if($user->notifications->delete()){
             return response()->json(['success'=>true, 'message'=>'deleted']);
+        }
+          
 
     }
 
