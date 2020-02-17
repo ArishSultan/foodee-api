ba0a8ac4e7dd0bf78d290040bbd6d70fda3856b9
Yasir <yasirameen92@gmail.com>
2020-02-17 22:07:24 +0500

    adsf

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -69,6 +69,9 @@ class SubscriptionController extends Controller
 //            $currentTime =Carbon::now();
             $startDate = $userExist->start_date;
             $endDate = $userExist->end_date;
+            return response()->json(['success'=>true, 'subscription'=>start_date, 'subscriptionEnd'=>$startDate,'message' => 'Your subscription is active']);
+
+
 
             if($startDate > $endDate)  {
                 return response()->json(['success'=>true, 'subscription'=>'active', 'subscriptionEnd'=>'End Date','message' => 'Your subscription is active']);
