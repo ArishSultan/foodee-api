38a85659956bd935f11837499b136d0c82e39c4f
Yasir <yasirameen92@gmail.com>
2020-02-17 20:58:59 +0500

    adsf

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/user/{id}', 'AuthController@user');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
